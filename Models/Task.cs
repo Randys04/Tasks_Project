6cd89fde8f02e9558ab890f560efbf32f5db5310
Randy Moya <stevenmoya02@gmail.com>
2023-09-19 14:36:51 -0600

    Se agregaron los modelos Category y Tasks y se agregaron los archivos README.md y .gitignore

---
+++ Models/Task.cs
@@ -0,0 +1,21 @@
+﻿namespace tasks_Project.Models
+{
+    public class Task
+    {
+        public Guid TaskId { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Priority TaskPriority { get; set; }
+        public DateTime CrreationDate { get; set; }
+    }
+
+    public enum Priority
+    {
+        Lowest,
+
+        Medium,
+
+        High
+    }
+}
