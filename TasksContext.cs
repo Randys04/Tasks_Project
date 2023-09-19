eb67ad55cc0d170ee308e3cfb0bf0948f43d3a4f
Randy Moya <stevenmoya02@gmail.com>
2023-09-19 14:58:58 -0600

    Se configuro el entity framework, para esto se creo el archivo TasksContext.cs

---
+++ TasksContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tasks_Project.Models;
+using Task = tasks_Project.Models.Task;
+
+namespace tasks_Project
+{
+    public class TasksContext: DbContext
+    {
+        public DbSet<Task> Tasks { get; set;}
+        public DbSet<Category> Categories { get; set;}
+
+        public TasksContext(DbContextOptions<TasksContext> options) : base(options) { }
+    }
+}
