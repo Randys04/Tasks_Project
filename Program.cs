b283c441632ffbafd7f4106f5555e3f8e1624d11
Randy Moya <stevenmoya02@gmail.com>
2023-09-20 14:14:05 -0600

    Se configuro la clase program para conectarse a una base de datos en memoria

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tasks_Project;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TasksContext>(p => p.UseInMemoryDatabase("TasksDB"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/dbConection", async ( [FromServices] TasksContext dbContext) => { 
+
+    dbContext.Database.EnsureCreated();
+    return Results.Ok($"Base de datos en memoria: {dbContext.Database.IsInMemory()}");
+});
 
 app.Run();
